5e59512b4374dcddec9e8f66ba9c64b49e2f0c8b
Amogh ganjikunta <88740564+amogyisabogy1@users.noreply.github.com>
2023-03-13 21:16:01 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,14 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.text) {
-      console.log(message.text)
-    }
-  });
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action == "highlight") {
+    chrome.windows.create({
+      url: chrome.runtime.getURL("popup.html"),
+      type: "popup",
+      width: 400,
+      height: 250,
+      left: screen.width - 450,
+      top: screen.height - 300
+    }, function(window) {
+      sendResponse({result: "success"});
+    });
+  }
+});
