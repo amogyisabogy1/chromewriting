5ce8a2c400677fc7f9f6351a89a1d02fd938df37
Amogh ganjikunta <88740564+amogyisabogy1@users.noreply.github.com>
2023-03-13 21:16:22 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,1 @@
-document.addEventListener('mouseup', function() {
-    var selectedText = window.getSelection().toString().trim();
-    if (selectedText.length > 0) {
-      chrome.runtime.sendMessage({text: selectedText});
-      var popup = window.open('popup.html', 'My Extension Popup', 'width=200,height=200');
-    }
-  });
+
