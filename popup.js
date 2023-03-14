0dc5aa8829fcf6da9b6e6821f248375a446f1f91
Amogh ganjikunta <88740564+amogyisabogy1@users.noreply.github.com>
2023-03-13 20:18:03 -0400

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Extension Popup</title>
+    <script src="popup.js"></script>
+  </head>
+  <body>
+    <button id="button1">Formalize</button>
+    <button id="button2">Extend</button>
+    <button id="button3">Shorten</button>
+  </body>
+</html>
